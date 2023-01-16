272c11e579f503fc43cbea86a99922334c75ecca
Esther Bauzou <exablob75@gmail.com>
2023-01-16 15:59:59 +0100

    Added checkbox & radio inputs

---
+++ src/components/molecules/CheckboxInput.vue
@@ -0,0 +1,116 @@
+<template>
+  <div
+    class="checkbox-group"
+    :class="{ small }"
+    @click.prevent="tickBox"
+  >
+    <input
+      type="checkbox"
+      :id="value"
+      :checked="modelValue.includes(value)"
+      :value="value"
+    >
+    <label :for="value">
+      <span class="tick"></span>
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: Array
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    required: true
+  },
+  small: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+function tickBox() {
+  // deep copy instead of shallow just in case
+  let newArray = JSON.parse(JSON.stringify(props.modelValue));
+  if (!props.modelValue.includes(props.value)) newArray.push(props.value);
+  else newArray = newArray.filter(item => item !== props.value);
+  emit("update:modelValue", newArray);
+}
+</script>
+
+<style lang="scss" scoped>
+.checkbox-group {
+  display: inline-block;
+
+  input {
+    display: none;
+
+    &:checked + label .tick {
+      background-color: var(--accent);
+      border-color: var(--accent);
+    }
+  }
+
+  label {
+    display: flex;
+    gap: 7px;
+    align-items: center;
+    cursor: pointer;
+    user-select: none;
+    font-weight: 500;
+
+    .tick {
+      display: inline-block;
+      box-sizing: border-box;
+      height: 18px;
+      width: 18px;
+      background-color: var(--intensified-bg);
+      border: 2px solid var(--text-standard);
+      border-radius: 4px;
+      position: relative;
+
+      &::before {
+        content: "";
+        position: absolute;
+        top: 20%;
+        left: 15%;
+        display: inline-block;
+        width: 8px;
+        height: 3px;
+        transform: rotate(-45deg);
+        border-bottom: 2px solid var(--accent-contraster);
+        border-left: 2px solid var(--accent-contraster);
+      }
+    }
+  }
+
+  &.small {
+    label {
+      font-size: .875rem;
+      gap: 5px;
+      font-weight: unset;
+
+      .tick {
+        height: 15px;
+        width: 15px;
+        border: 1px solid var(--text-standard);
+        border-radius: 3px;
+
+        &::before {
+          width: 7px;
+          top: 25%;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/molecules/RadioInput.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    class="radio-group"
+    :class="{ small }"
+    @click.prevent="toggle"
+  >
+    <input
+      type="radio"
+      :name="groupName"
+      :id="value"
+      :checked="modelValue === value"
+      :value="value"
+    >
+    <label :for="value">{{ label }}</label>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String
+  },
+  groupName: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    required: true
+  },
+  small: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+function toggle() {
+  emit("update:modelValue", props.value);
+}
+</script>
+
+<style lang="scss" scoped>
+.radio-group {
+  display: inline-block;
+
+  input {
+    display: none;
+
+    &:checked + label::before {
+      border: 2px solid var(--intensified-bg);
+      background-color: var(--accent);
+      outline-color: var(--accent);
+    }
+  }
+
+  label {
+    display: flex;
+    gap: 5px;
+    align-items: center;
+    cursor: pointer;
+    user-select: none;
+    font-weight: 500;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      box-sizing: border-box;
+      height: 15px;
+      width: 15px;
+      background-color: var(--intensified-bg);
+      outline: 2px solid var(--text-standard);
+      border-radius: 50px;
+    }
+  }
+
+  &.small {
+    label {
+      font-size: .875rem;
+      gap: 5px;
+      font-weight: unset;
+
+      &::before {
+        height: 13px;
+        width: 13px;
+        outline: 1px solid var(--text-standard);
+      }
+    }
+  }
+}
+</style>
