5283c57f00963ad792422b69fbedc5876289101f
Esther Bauzou <exablob75@gmail.com>
2023-04-10 22:58:02 +0200

    Added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+      manifest: {
+        name: 'Whistle Buddy',
+        short_name: 'Whistle Buddy',
+        description: 'Learning companion for Irish Whistle fingerings',
+        theme_color: '#ede1d9',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
