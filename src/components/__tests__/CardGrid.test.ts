4dbd6f4b3b23f7d9d87aa0b76e5d0591c0b9d708
Esther Bauzou <exablob75@gmail.com>
2023-04-13 12:56:13 +0200

    Added cpn tests

--- src/components/__tests__/CardGrid.test.ts
+++ src/components/__tests__/CardGrid.test.ts
@@ -1,9 +1,9 @@
-import { test, expect } from "vitest";
+import { describe, test, expect } from "vitest";
 import { shallowMount } from "@vue/test-utils";
 import CardGrid from "@/components/CardGrid.vue";
 import { useParamsStore } from "@/stores/params";
 import { createPinia } from "pinia";
-import type { ComponentPublicInstance } from "vue"
+import type { ComponentPublicInstance } from "vue";
 import type { ICard } from "@/types/MusicalDataTypes";
 
 interface CustomCpnInstance extends ComponentPublicInstance {
@@ -40,50 +40,52 @@ const cards: ICard[] = [
   },
 ];
 
-test("renders grid and pagination correctly", () => {
-  const pinia = createPinia();
-  useParamsStore(pinia);
-
-  const wrapper = shallowMount(CardGrid, {
-    global: {
-      plugins: [pinia],
-    },
-    props: {
-      cards,
-      pagination: true,
-    },
+describe("CardGrid.vue", () => {
+  test("renders grid and pagination correctly", () => {
+    const pinia = createPinia();
+    useParamsStore(pinia);
+  
+    const wrapper = shallowMount(CardGrid, {
+      global: {
+        plugins: [pinia],
+      },
+      props: {
+        cards,
+        pagination: true,
+      },
+    });
+  
+    expect(wrapper.find(".grid").exists()).toBe(true);
+    expect(wrapper.find(".pagination").exists()).toBe(true);
   });
-
-  expect(wrapper.find(".grid").exists()).toBe(true);
-  expect(wrapper.find(".pagination").exists()).toBe(true);
-});
-
-test("pagination controls work correctly", async () => {
-  const pinia = createPinia();
-  const paramsStore = useParamsStore(pinia);
-
-  const wrapper = shallowMount(CardGrid, {
-    global: {
-      plugins: [pinia],
-    },
-    props: {
-      cards,
-      pagination: true,
-    },
+  
+  test("pagination controls work correctly", async () => {
+    const pinia = createPinia();
+    const paramsStore = useParamsStore(pinia);
+  
+    const wrapper = shallowMount(CardGrid, {
+      global: {
+        plugins: [pinia],
+      },
+      props: {
+        cards,
+        pagination: true,
+      },
+    });
+    const vm = wrapper.vm as CustomCpnInstance;
+  
+    paramsStore.fingTableParams.cardsPerPage = 1;
+  
+    const prevButton = wrapper.find(".pagination-prev");
+    const nextButton = wrapper.find(".pagination-next");
+  
+    await nextButton.trigger("click");
+    expect(vm.currentPage).toBe(2);
+  
+    await prevButton.trigger("click");
+    expect(vm.currentPage).toBe(1);
+  
+    await prevButton.trigger("click");
+    expect(vm.currentPage).toBe(3);
   });
-  const vm = wrapper.vm as CustomCpnInstance;
-
-  paramsStore.fingTableParams.cardsPerPage = 1;
-
-  const prevButton = wrapper.find(".pagination-prev");
-  const nextButton = wrapper.find(".pagination-next");
-
-  await nextButton.trigger("click");
-  expect(vm.currentPage).toBe(2);
-
-  await prevButton.trigger("click");
-  expect(vm.currentPage).toBe(1);
-
-  await prevButton.trigger("click");
-  expect(vm.currentPage).toBe(3);
 });
---
+++ src/components/__tests__/ChoiceGrid.test.ts
@@ -0,0 +1,78 @@
+import { describe, test, expect, beforeEach } from "vitest";
+import { shallowMount, VueWrapper } from "@vue/test-utils";
+import ChoiceGrid from "@/components/ChoiceGrid.vue";
+import Card from "@/components/molecules/Card.vue";
+import type { ComponentPublicInstance } from "vue";
+import type { ICard } from "@/types/MusicalDataTypes";
+
+interface CustomCpnInstance extends ComponentPublicInstance {
+  selectedCard: ICard
+}
+
+const cards: ICard[] = [
+  {
+    id: 1,
+    name: { en: "d", fr: "ré" },
+    fingerings: [{
+      id: 1, posInScale: 1,  holes: [ 2, 2, 2, 2, 2, 2 ], type: "standard", octaves: [ 1 ]
+    }],
+    octave: 1,
+    soundUrl: "3_d_1.mp3"
+  },
+  {
+    id: 2,
+    name: { en: "d", fr: "ré" },
+    fingerings: [{
+      id: 3, posInScale: 3,  holes: [ 2, 2, 2, 0, 0, 0 ], type: "standard", octaves: [ 1 ]
+    }],
+    octave: 2,
+    soundUrl: "5_e_1.mp3"
+  },
+  {
+    id: 3,
+    name: { en: "d", fr: "ré" },
+    fingerings: [{
+      id: 5, posInScale: 5,  holes: [ 2, 2, 2, 2, 0, 0 ], type: "specific", octaves: [ 1 ]
+    }],
+    octave: 1,
+    soundUrl: "7_f#_gb_1.mp3"
+  },
+];
+
+describe("ChoiceGrid.vue", () => {
+  let wrapper: VueWrapper;
+
+  const defaultProps = { cards };
+
+  beforeEach(() => {
+    wrapper = shallowMount(ChoiceGrid, {
+      props: defaultProps,
+    });
+  });
+
+  test("renders correct number of Card components", () => {
+    const cardComponents = wrapper.findAllComponents(Card);
+    expect(cardComponents.length).toBe(defaultProps.cards.length);
+  });
+
+  test("assigns correct classes based on cards prop length", async () => {
+    await wrapper.setProps({
+      cards: defaultProps.cards,
+      valuesToShow: "notes",
+    });
+
+    const grid = wrapper.get(".grid");
+    expect(grid.classes()).toContain("grid-note-3");
+  });
+
+  test("updates selectedCard when @cardClicked is emitted", async () => {
+    const cardComponents = wrapper.findAllComponents(Card);
+    const firstCard = cardComponents[0];
+    const firstCardId = firstCard.props("note").id;
+    const vm = wrapper.vm as CustomCpnInstance;
+
+    await firstCard.vm.$emit("@cardClicked", firstCard.props("note"));
+
+    expect(vm.selectedCard.id).toBe(firstCardId);
+  });
+});
